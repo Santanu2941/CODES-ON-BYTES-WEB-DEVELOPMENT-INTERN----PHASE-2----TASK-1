7b305437ca30f401d085cb51f4e0890068bf23ca
Santanu Ghosh <131514748+Santanu2941@users.noreply.github.com>
2023-12-20 20:35:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_\+/?|"; // Include any additional characters you want
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+
+    document.getElementById("passwordOutput").value = password;
+}
